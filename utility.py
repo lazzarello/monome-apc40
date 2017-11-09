1757d44dcccc63e21d7f8a9b5c7deffd6c77b2ea
Lee Azzarello <lee@seriesdigital.com>
2017-11-09 15:44:30 -0800

    add a bunch of MIDI device/socket example code

--- utility.py
+++ utility.py
@@ -2,13 +2,53 @@
 
 # MIDI library
 # https://github.com/vishnubob/python-midi
-import midi
+import midi.sequencer as sequencer
 # OSC library
 # https://pypi.python.org/pypi/python-osc
 from pythonosc import osc_message_builder
 # https://github.com/monome/libmonome
 # https://monome.org/docs/grid-studies/python/
 #import monome
+
+client = "APC40"
+port = "APC40"
+
+mode = { "ableton mode" : 1,
+         "ableton alt mode" : 2,
+         "generic mode" : 0
+       }
+
+# initialize the an ALSA sequencer object
+hardware = sequencer.SequencerHardware()
+
+if not client.isdigit:
+    client = hardware.get_client(client)
+
+if not port.isdigit:
+    port = hardware.get_port(port)
+
+class ReadLoop:
+    seq = sequencer.SequencerRead(sequencer_resolution=120)
+    seq.subscribe_port(0, 0)
+    seq.start_sequencer()
+    while True:
+      event = seq.event_read()
+      if event is not None:
+          print event
+
+class WriteLoop:
+    seq = sequencer.SequencerWrite(sequencer_resolution=pattern.resolution)
+    seq.subscribe_port(client, port)
+    seq.start_sequencer()
+    for event in events:
+        buf = seq.event_write(event, False, False, True)
+        if buf == None:
+            continue
+        if buf < 1000:
+            time.sleep(.5)
+    while event.tick > seq.queue_get_tick_time():
+        seq.drain()
+        time.sleep(.5)
 
 apc40_x = [1,2,3,4,5,6,7,8] # MIDI Channel
 apc40_y = [53,54,55,56,57,52,51,50] # MIDI note number
@@ -26,6 +66,8 @@ def midi_to_monome(event):
     y = apc40_y.index(event[0]) + 1
     state = event[2]
     return [x,y,state]
+
+set_mode(mode["ableton mode"])
 
 print midi_to_monome([52,3,0])
 
